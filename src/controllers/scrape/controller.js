58daf9494a0c2128bbbb8be70edea8c2ece7066d
Jai <jksmithnyc@gmail.com>
2020-10-11 02:10:21 -0600

    removed post-write res.send

--- src/controllers/scrape/controller.js
+++ src/controllers/scrape/controller.js
@@ -14,6 +14,14 @@ const scrape = async (req, res) => {
     return res.status(400).send({ err: `invalid scrape type: '${type}'` });
   }
 
+  // load most recent data from repo
+  await loadCurrent()
+    .catch((err) => {
+      console.error(err.stack);
+
+      return res.status(500).send({ err: 'Failed to load repo' });
+    });
+
   // get appropriate  methods
   const { fetch, parse } = getMethodsForType(type);
 
@@ -22,15 +30,12 @@ const scrape = async (req, res) => {
     .catch((err) => {
       console.error(err.stack);
 
-      return res.status(500).send({ err: `Error fetching ${type}` });
-    });
-
-  // load most recent data from repo
-  await loadCurrent()
-    .catch((err) => {
-      console.error(err.stack);
-
-      return res.status(500).send({ err: 'Failed to load repo' });
+      if (!res.headersSent) {
+        return res.status(500).send({ err: `Error fetching ${type}` });
+      } else {
+        res.write({ err: `Error fetching ${type}` });
+        return res.end();
+      }
     });
 
   // load versions file
@@ -40,7 +45,12 @@ const scrape = async (req, res) => {
 
   // check for changes, return early if none
   if (versions.current[type]?.hash === hash) {
-    return res.send({ msg: `No changes detected for ${type}` });
+    if (!res.headersSent) {
+      return res.send({ msg: `No changes detected for ${type}` });
+    } else {
+      res.write({ msg: `No changes detected for ${type}` });
+      return res.end();
+    }
   }
 
   // parse raw data and write to local
@@ -70,10 +80,20 @@ const scrape = async (req, res) => {
     .catch((err) => {
       console.error(err.stack);
 
-      return res.status(500).send({ err: 'Failed to update repo' });
+      if (!res.headersSent) {
+        return res.status(500).send({ err: 'Failed to update repo' });
+      } else {
+        res.write({ err: 'Failed to update repo' });
+        return res.end();
+      }
     });
 
-  return res.send({ msg: `Changes detected and pushed for ${type}` });
+  if (!res.headersSent) {
+    return res.send({ msg: `Changes detected and pushed for ${type}` });
+  } else {
+    res.write({ msg: `Changes detected and pushed for ${type}` });
+    return res.end();
+  }
 };
 
 const ScrapeController = {
